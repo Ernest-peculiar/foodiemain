13d98f340f0f4320e24e1f5077559563569923a2
Ernest Peculiar <ihechigideon28@gmail.com>
2026-08-28 23:02:12 -0700

    refactor test cases in normalize-phone.test.js to use consistent double quotes

--- tests/normalize-phone.test.js
+++ tests/normalize-phone.test.js
@@ -1,15 +1,15 @@
-const test = require('node:test');
-const assert = require('node:assert/strict');
-const { normalizePhone } = require('../lib/utils');
+const test = require("node:test");
+const assert = require("node:assert/strict");
+const { normalizePhone } = require("../lib/utils");
 
-test('normalizePhone converts Nigerian local numbers to WhatsApp format', () => {
-  assert.equal(normalizePhone('08012345678'), '2348012345678');
-  assert.equal(normalizePhone('+2348012345678'), '2348012345678');
-  assert.equal(normalizePhone('2348012345678'), '2348012345678');
-  assert.equal(normalizePhone('080 123 456 78'), '2348012345678');
+test("normalizePhone converts Nigerian local numbers to WhatsApp format", () => {
+  assert.equal(normalizePhone("08012345678"), "2348012345678");
+  assert.equal(normalizePhone("+2348012345678"), "2348012345678");
+  assert.equal(normalizePhone("2348012345678"), "2348012345678");
+  assert.equal(normalizePhone("080 123 456 78"), "2348012345678");
 });
 
-test('normalizePhone leaves empty input empty', () => {
-  assert.equal(normalizePhone(''), '');
-  assert.equal(normalizePhone(null), '');
+test("normalizePhone leaves empty input empty", () => {
+  assert.equal(normalizePhone(""), "");
+  assert.equal(normalizePhone(null), "");
 });
